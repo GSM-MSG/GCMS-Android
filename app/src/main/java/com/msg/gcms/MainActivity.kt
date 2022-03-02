34ffd2d6643c509664c286d88c14c241621fee71
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-03-03 07:57:09 +0900

    [CREATE] Project setting

---
+++ app/src/main/java/com/msg/gcms/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.msg.gcms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.msg.msg_gcms.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
