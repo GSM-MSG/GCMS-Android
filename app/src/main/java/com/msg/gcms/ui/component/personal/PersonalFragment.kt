0059ae8546d21f86435fc54073db455e39960305
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-04 23:33:23 +0900

    📝 :: bottom navigation

---
+++ app/src/main/java/com/msg/gcms/ui/component/personal/PersonalFragment.kt
@@ -0,0 +1,11 @@
+package com.msg.gcms.ui.component.personal
+
+import com.msg.gcms.R
+import com.msg.gcms.databinding.FragmentPersonalBinding
+import com.msg.gcms.ui.base.BaseFragment
+
+class PersonalFragment: BaseFragment<FragmentPersonalBinding>(R.layout.fragment_personal) {
+    override fun init() {
+
+    }
+}
