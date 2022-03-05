32463e0858c8ee193a2c82de22d3b5ec8bae30ed
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-05 14:04:20 +0900

    📝 :: clubFragment

--- app/src/main/java/com/msg/gcms/ui/component/main/PagerAdapter.kt
+++ app/src/main/java/com/msg/gcms/ui/component/main/PagerAdapter.kt
@@ -4,18 +4,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.msg.gcms.ui.component.free.FreeFragment
-import com.msg.gcms.ui.component.major.MajorFragment
-import com.msg.gcms.ui.component.personal.PersonalFragment
+import com.msg.gcms.ui.component.club.ClubFragment
 
 class PagerAdapter(fm: FragmentManager, lc: Lifecycle): FragmentStateAdapter(fm, lc) {
     override fun getItemCount() = 3
 
     override fun createFragment(position: Int): Fragment {
         return when(position){
-            0 -> MajorFragment()
-            1 -> FreeFragment()
-            2 -> PersonalFragment()
+            0 -> ClubFragment()
+            1 -> ClubFragment()
+            2 -> ClubFragment()
             else -> error("no such position: $position")
         }
     }
