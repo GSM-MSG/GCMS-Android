a8e10d356ddcf2a375bc1cd349e8197a793220a2
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-03-07 12:09:08 +0900

    ⚙ announcement 페이지 XML 작성

---
+++ app/src/main/java/com/msg/gcms/ui/component/announcement/AnnouncementFragment.kt
@@ -0,0 +1,12 @@
+package com.msg.gcms.ui.component.announcement
+
+import com.msg.gcms.R
+import com.msg.gcms.databinding.FragmentAnnouncementBinding
+import com.msg.gcms.ui.base.BaseFragment
+
+class AnnouncementFragment : BaseFragment<FragmentAnnouncementBinding> (R.layout.fragment_announcement) {
+    override fun init() {
+
+    }
+
+}
