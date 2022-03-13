815936ad009a5bed6d36c830f25028d7ceb40039
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-13 15:25:28 +0900

    📝 :: club fragment

---
+++ app/src/main/java/com/msg/gcms/ui/component/club/home/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.msg.gcms.ui.component.club.home
+
+import com.msg.gcms.R
+import com.msg.gcms.databinding.FragmentClubHomeBinding
+import com.msg.gcms.ui.base.BaseFragment
+
+class HomeFragment: BaseFragment<FragmentClubHomeBinding>(R.layout.fragment_club_home) {
+    override fun init() {
+        click()
+    }
+
+    private fun click(){
+
+    }
+}
