5cb5680ac856540936beead8d7c7c744c7b4bfc9
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-13 15:25:28 +0900

    📝 :: club fragment

---
+++ app/src/main/java/com/msg/gcms/ui/component/club/detail/DetailFragment.kt
@@ -0,0 +1,11 @@
+package com.msg.gcms.ui.component.club.detail
+
+import com.msg.gcms.R
+import com.msg.gcms.databinding.FragmentClubDetailBinding
+import com.msg.gcms.ui.base.BaseFragment
+
+class DetailFragment: BaseFragment<FragmentClubDetailBinding>(R.layout.fragment_club_detail) {
+    override fun init() {
+
+    }
+}
