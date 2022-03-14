f0ed3845727a4a5e3f209d3a45e00e4181547415
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-03-14 10:20:30 +0900

    ⚙ :: AppIcon setting

--- app/src/main/java/com/msg/gcms/ui/component/clubmaker/MakeClubFragment.kt
+++ app/src/main/java/com/msg/gcms/ui/component/clubmaker/MakeClubFragment.kt
@@ -1,11 +1,15 @@
 package com.msg.gcms.ui.component.clubmaker
 
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.msg.gcms.R
 import com.msg.gcms.databinding.FragmentMakeClubBinding
 import com.msg.gcms.ui.base.BaseFragment
+import com.msg.viewmodel.MainViewModel
 
 class MakeClubFragment : BaseFragment<FragmentMakeClubBinding>(R.layout.fragment_make_club ), View.OnClickListener {
+
+    private val mainViewModel by activityViewModels<MainViewModel>()
 
     override fun init() {
 
--- app/src/main/java/com/msg/gcms/ui/component/main/MainActivity.kt
+++ app/src/main/java/com/msg/gcms/ui/component/main/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.msg.gcms.ui.component.main
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.view.MenuItem
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.activity.viewModels
 import com.msg.gcms.R
 import com.msg.gcms.databinding.ActivityMainBinding
 import com.msg.gcms.ui.base.BaseActivity
+import com.msg.viewmodel.MainViewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    private val mainViewModel by viewModels<MainViewModel>()
+
     override fun viewSetting() {
         initBottomNav()
     }
@@ -23,27 +23,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
     private fun initBottomNav() {
-        binding.fragmentView.apply {
-            adapter = PagerAdapter(supportFragmentManager, lifecycle)
-            registerOnPageChangeCallback(PageChangeCallback(binding.bottomNavigation))
-        }
-        binding.bottomNavigation.setOnNavigationItemSelectedListener { navSelected(it) }
-    }
 
-    private fun navSelected(item: MenuItem): Boolean {
-        val checked = item.setChecked(true)
-        when (checked.itemId) {
-            R.id.majorFragment -> {
-                binding.fragmentView.currentItem = 0
-            }
-            R.id.freeFragment -> {
-                binding.fragmentView.currentItem = 1
-            }
-            R.id.personalFragment -> {
-                binding.fragmentView.currentItem = 2
-            }
-        }
-        return false
     }
 
 
