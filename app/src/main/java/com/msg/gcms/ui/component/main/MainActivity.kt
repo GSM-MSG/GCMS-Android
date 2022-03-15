1a82f3733ac071d778321612c5bbd8125bc806ac
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-15 20:58:30 +0900

    📝 :: remove viewpager

--- app/src/main/java/com/msg/gcms/ui/component/main/MainActivity.kt
+++ app/src/main/java/com/msg/gcms/ui/component/main/MainActivity.kt
@@ -1,30 +1,44 @@
 package com.msg.gcms.ui.component.main
 
-import android.view.MenuItem
+import android.content.Intent
+import android.util.Log
 import androidx.activity.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.msg.gcms.R
 import com.msg.gcms.databinding.ActivityMainBinding
 import com.msg.gcms.ui.base.BaseActivity
+import com.msg.gcms.ui.component.profile.ProfileActivity
 import com.msg.viewmodel.MainViewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-
-    private val mainViewModel by viewModels<MainViewModel>()
-
+    private val mainViewModel by viewModels<MainViewModel> ()
     override fun viewSetting() {
         initBottomNav()
+        clickProfile()
     }
 
     override fun observeEvent() {
-        mainViewModel.clubName.observe(this) {
+        mainViewModel.clubName.observe(this, {
             binding.clubNameTxt.text = it
-        }
-
+        })
     }
 
     private fun initBottomNav() {
-
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.fragment_club)?.findNavController()
+        val nav = binding.bottomNavigation as BottomNavigationView
+        navController?.let {
+            nav.setupWithNavController(navController)
+        }
     }
 
-
+    private fun clickProfile(){
+        binding.profileBtn.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
+            startActivity(intent)
+        }
+    }
 }
