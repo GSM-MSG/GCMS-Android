bd60ed031d8cefca5cd19942df43acca5ad2a504
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-03-19 15:05:57 +0900

    ⚙ :: LoginActivity XML

--- app/src/main/java/com/msg/gcms/ui/component/intro/IntroActivity.kt
+++ app/src/main/java/com/msg/gcms/ui/component/intro/IntroActivity.kt
@@ -4,8 +4,9 @@ package com.msg.gcms.ui.component.intro
 import android.content.Intent
 import com.msg.gcms.R
 import com.msg.gcms.ui.base.BaseActivity
-import com.msg.gcms.ui.component.main.MainActivity
 import com.msg.gcms.databinding.ActivityIntroBinding
+import com.msg.gcms.ui.component.login.LoginActivity
+import com.msg.gcms.ui.component.registration.RegistrationActivity
 
 class IntroActivity : BaseActivity<ActivityIntroBinding>(R.layout.activity_intro) {
     override fun viewSetting() {
@@ -14,12 +15,21 @@ class IntroActivity : BaseActivity<ActivityIntroBinding>(R.layout.activity_intro
 
     override fun observeEvent() {
         clickLogin()
+        clickRegistration()
     }
 
     private fun clickLogin() {
         binding.loginBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+            finish()
+        }
+    }
+
+    private fun clickRegistration() {
+        binding.signInBtn.setOnClickListener {
+            val registrationIntent = Intent(this, RegistrationActivity::class.java)
+            startActivity(registrationIntent)
             finish()
         }
     }
