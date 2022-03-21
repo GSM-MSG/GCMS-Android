dac86b70bab7cc78ac5d5f01176755dd80c141f5
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-21 12:07:52 +0900

    ⚡️ :: login animation

--- app/src/main/java/com/msg/gcms/ui/component/login/LoginActivity.kt
+++ app/src/main/java/com/msg/gcms/ui/component/login/LoginActivity.kt
@@ -1,13 +1,13 @@
 package com.msg.gcms.ui.component.login
 
 import android.content.Intent
+import android.os.Handler
 import android.view.View
-import androidx.core.view.isEmpty
+import android.view.animation.AnimationUtils
 import com.msg.gcms.R
 import com.msg.gcms.databinding.ActivityLoginBinding
 import com.msg.gcms.ui.base.BaseActivity
 import com.msg.gcms.ui.component.intro.IntroActivity
-import com.msg.gcms.ui.component.main.MainActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login),
     View.OnClickListener {
@@ -16,37 +16,17 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
         binding.apply {
             backBtn.setOnClickListener(this@LoginActivity)
             findPasswordBtn.setOnClickListener(this@LoginActivity)
-            loginBtn.setOnClickListener(this@LoginActivity)
         }
+        setAnim()
     }
 
     override fun observeEvent() {
 
     }
 
-    private fun editTextCheck() {
-        if (binding.emailEt.text!!.isEmpty()) {
-            binding.textInputLayout.error = "이메일을 입력해주세요"
-        }
-        else if (binding.emailEt.text!!.isNotEmpty()) {
-            binding.textInputLayout.error = null
-        }
-        if (binding.passwordEt.text!!.isEmpty()) {
-            binding.textInputLayout2.error = "비밀번호를 입력해주세요"
-        }
-        else if (binding.passwordEt.text!!.isNotEmpty()) {
-            binding.textInputLayout2.error = null
-        }
-        if (binding.emailEt.text!!.isNotEmpty() && binding.passwordEt.text!!.isNotEmpty()) {
-            val mainIntent = Intent(this,MainActivity::class.java)
-            startActivity(mainIntent)
-            finish()
-        }
-    }
-
 
     override fun onClick(v: View?) {
-        when (v!!.id) {
+        when(v!!.id) {
             binding.backBtn.id -> {
                 val introIntent = Intent(this, IntroActivity::class.java)
                 startActivity(introIntent)
@@ -55,8 +35,15 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
             binding.findPasswordBtn.id -> {
                 shortToast("업데이트 예정입니다.")
             }
-            binding.loginBtn.id ->
-                editTextCheck()
+        }
+    }
+
+    private fun setAnim(){
+        binding.apply {
+            wave1.startAnimation(AnimationUtils.loadAnimation(this@LoginActivity, R.anim.login_animation1))
+            Handler().postDelayed({
+                wave2.startAnimation(AnimationUtils.loadAnimation(this@LoginActivity, R.anim.login_animation2))
+            }, 1000)
         }
     }
 }
