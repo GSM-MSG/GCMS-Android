bd292c7c534a5e813acd78a38c34bdfcb42d4685
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-21 12:07:52 +0900

    ⚡️ :: login animation

--- app/src/main/java/com/msg/gcms/ui/component/login/LoginActivity.kt
+++ app/src/main/java/com/msg/gcms/ui/component/login/LoginActivity.kt
@@ -2,8 +2,6 @@ package com.msg.gcms.ui.component.login
 
 import android.content.Intent
 import android.os.Handler
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import android.view.animation.AnimationUtils
 import com.msg.gcms.R
@@ -18,10 +16,8 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
         binding.apply {
             backBtn.setOnClickListener(this@LoginActivity)
             findPasswordBtn.setOnClickListener(this@LoginActivity)
-            loginBtn.setOnClickListener(this@LoginActivity)
         }
         setAnim()
-        setTyping()
     }
 
     override fun observeEvent() {
@@ -30,7 +26,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
 
 
     override fun onClick(v: View?) {
-        when (v!!.id) {
+        when(v!!.id) {
             binding.backBtn.id -> {
                 val introIntent = Intent(this, IntroActivity::class.java)
                 startActivity(introIntent)
@@ -39,72 +35,15 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
             binding.findPasswordBtn.id -> {
                 shortToast("업데이트 예정입니다.")
             }
-            binding.loginBtn.id -> {
-                binding.errorMessage.visibility = View.VISIBLE
-                binding.idEditText.background = getDrawable(R.drawable.bg_login_edit_text_error)
-                binding.textInputLayout.startAnimation(
-                    AnimationUtils.loadAnimation(
-                        this@LoginActivity,
-                        R.anim.edit_text_error_animation
-                    )
-                )
-                binding.pwEditText.background = getDrawable(R.drawable.bg_login_edit_text_error)
-                binding.textInputLayout2.startAnimation(
-                    AnimationUtils.loadAnimation(
-                        this@LoginActivity,
-                        R.anim.edit_text_error_animation
-                    )
-                )
-                Handler().postDelayed({
-                    binding.errorMessage.visibility = View.GONE
-                    binding.idEditText.background = getDrawable(R.drawable.bg_login_edit_text)
-                    binding.pwEditText.background = getDrawable(R.drawable.bg_login_edit_text)
-                }, 500)
-            }
         }
     }
 
-    private fun setAnim() {
+    private fun setAnim(){
         binding.apply {
-            wave1.startAnimation(
-                AnimationUtils.loadAnimation(
-                    this@LoginActivity,
-                    R.anim.login_animation1
-                )
-            )
+            wave1.startAnimation(AnimationUtils.loadAnimation(this@LoginActivity, R.anim.login_animation1))
             Handler().postDelayed({
-                wave2.startAnimation(
-                    AnimationUtils.loadAnimation(
-                        this@LoginActivity,
-                        R.anim.login_animation2
-                    )
-                )
-            }, 800)
+                wave2.startAnimation(AnimationUtils.loadAnimation(this@LoginActivity, R.anim.login_animation2))
+            }, 1000)
         }
-    }
-
-    private fun setTyping() {
-        binding.idEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-                binding.textInputLayout.isHintEnabled = p0!!.length <= 0
-            }
-        })
-        binding.pwEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-                binding.textInputLayout2.isHintEnabled = p0!!.length <= 0
-            }
-        })
     }
 }
