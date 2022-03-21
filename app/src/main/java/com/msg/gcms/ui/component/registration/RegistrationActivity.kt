9eed3172f8c147b6f59d97fa7a6fe7fb2cfddf2d
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-03-22 00:22:52 +0900

    📑 :: EditTextCheck Logic functionalize

--- app/src/main/java/com/msg/gcms/ui/component/registration/RegistrationActivity.kt
+++ app/src/main/java/com/msg/gcms/ui/component/registration/RegistrationActivity.kt
@@ -9,39 +9,47 @@ import com.msg.gcms.ui.component.intro.IntroActivity
 import com.msg.gcms.ui.component.main.MainActivity
 
 class RegistrationActivity :
-    BaseActivity<ActivityRegistrationBinding>(R.layout.activity_registration), View.OnClickListener {
+    BaseActivity<ActivityRegistrationBinding>(R.layout.activity_registration),
+    View.OnClickListener {
 
 
     override fun viewSetting() {
         binding.apply {
             backBtn.setOnClickListener(this@RegistrationActivity)
+            emailAccessBtn.setOnClickListener(this@RegistrationActivity)
         }
     }
 
     override fun observeEvent() {
     }
 
+    private fun editTextCheck() {
+        if (binding.emailEt.text!!.isEmpty()) {
+            binding.textInputLayout.error = "이메일을 입력해주세요"
+        } else if (binding.emailEt.text!!.isNotEmpty()) {
+            binding.textInputLayout.error = null
+        }
+        if (binding.passwordEt.text!!.isEmpty()) {
+            binding.textInputLayout2.error = "비밀번호를 입력해주세요"
+        } else if (binding.passwordEt.text!!.isNotEmpty()) {
+            binding.textInputLayout2.error = null
+        }
+        if (binding.emailEt.text!!.isNotEmpty() && binding.passwordEt.text!!.isNotEmpty()) {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }
+    }
+
     override fun onClick(v: View?) {
-        when(v!!.id) {
+        when (v!!.id) {
             binding.backBtn.id -> {
                 val introIntent = Intent(this, IntroActivity::class.java)
                 startActivity(introIntent)
                 finish()
             }
-            binding.emailAccessBtn.id -> {
-                if (binding.emailEt.text!!.isEmpty()) {
-                    binding.textInputLayout.error = "이메일을 입력해주세요"
-                }
-                else if (binding.emailEt.text!!.isNotEmpty()) {
-                    binding.textInputLayout.error = null
-                }
-                if (binding.passwordEt.text!!.isEmpty()) {
-                    binding.textInputLayout2.error = "비밀번호를 입력해주세요"
-                }
-                else if (binding.passwordEt.text!!.isNotEmpty()) {
-                    binding.textInputLayout2.error = null
-                }
-            }
+            binding.emailAccessBtn.id ->
+                editTextCheck()
         }
     }
 
