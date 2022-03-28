17cb61979907e995f2cc497fa91e17f96bfac763
baegteun <baegteun@gmail.com>
2022-03-28 20:19:25 +0900

    :recycle: :: EmailCheck
    viewModel, setOnClickListner

--- app/src/main/java/com/msg/viewmodel/RegistrationViewModel.kt
+++ app/src/main/java/com/msg/viewmodel/RegistrationViewModel.kt
@@ -1,12 +1,17 @@
 package com.msg.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.msg.gcms.ui.base.BaseViewModel
 import kotlinx.coroutines.launch
 
-class RegistrationViewModel constructor() : BaseViewModel() {
+class RegistrationViewModel : BaseViewModel(), LifecycleObserver {
+     private var _emailCode = MutableLiveData<String>().apply { value = "" }
+    var emailCode: LiveData<String> = _emailCode
+
     fun registrationLogic(email: String, password: String) {
         val queries: HashMap<String, String> = HashMap()
 
@@ -20,5 +25,20 @@ class RegistrationViewModel constructor() : BaseViewModel() {
 
     fun emailCheckLogic(emailCode: ArrayList<String>) {
 
+    }
+
+    fun typeNumber(num: Int) {
+        if (_emailCode.value?.length ?: 0 < 4) {
+            val sb = StringBuilder(_emailCode.value)
+            sb.append(num.toString())
+            _emailCode.postValue(sb.toString())
+        }
+    }
+    fun eraseNumber() {
+        if (_emailCode.value?.isNotEmpty() == true) {
+            val sb = StringBuilder(_emailCode.value)
+            sb.deleteCharAt(sb.length-1)
+            _emailCode.postValue(sb.toString())
+        }
     }
 }
