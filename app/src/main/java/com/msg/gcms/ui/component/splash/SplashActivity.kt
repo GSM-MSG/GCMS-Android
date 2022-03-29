7700e276cef0abcc0a58fb5a6491e9eca1cddd90
김현승 <80810303+khs3994@users.noreply.github.com>
2022-03-29 23:07:48 +0900

    📝 :: 스플래시 화면 구현

--- app/src/main/java/com/msg/gcms/ui/component/splash/SplashActivity.kt
+++ app/src/main/java/com/msg/gcms/ui/component/splash/SplashActivity.kt
@@ -3,16 +3,10 @@ package com.msg.gcms.ui.component.splash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.msg.gcms.R
-import com.msg.gcms.databinding.ActivitySplashBinding
-import com.msg.gcms.ui.base.BaseActivity
 
-class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
-    override fun viewSetting() {
-
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
     }
-
-    override fun observeEvent() {
-
-    }
-
 }
