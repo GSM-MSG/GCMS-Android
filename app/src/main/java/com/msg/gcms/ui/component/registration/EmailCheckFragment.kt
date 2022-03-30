8e424459d7c78832fba5dfd701c25ff301fa157e
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-03-30 23:25:46 +0900

    Edit Email Check Fragment EditText, Add Email Check Fragment in Registration graph

--- app/src/main/java/com/msg/gcms/ui/component/registration/EmailCheckFragment.kt
+++ app/src/main/java/com/msg/gcms/ui/component/registration/EmailCheckFragment.kt
@@ -1,17 +1,30 @@
 package com.msg.gcms.ui.component.registration
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.msg.gcms.R
 import com.msg.gcms.databinding.FragmentEmailCheckBinding
 import com.msg.gcms.ui.base.BaseFragment
 
 
-class EmailCheckFragment : BaseFragment<FragmentEmailCheckBinding>(R.layout.fragment_email_check) {
+class EmailCheckFragment : BaseFragment<FragmentEmailCheckBinding>(R.layout.fragment_email_check), View.OnClickListener {
     override fun init() {
+        viewSetting()
 
+    }
+
+    private fun viewSetting() {
+        binding.apply {
+            backBtn.setOnClickListener(this@EmailCheckFragment)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            binding.backBtn.id -> {
+                this.findNavController().navigate(R.id.action_emailCheckFragment_to_signUpFragment)
+            }
+        }
     }
 }
