93d90284d5555b0a2a72a96910f0d28e136eca55
김현승 <80810303+khs3994@users.noreply.github.com>
2022-03-31 22:35:33 +0900

    📝 :: BaseRequest 생성

--- app/src/main/java/com/msg/gcms/data/remote/dto/datasource/base/BaseRequest.kt
+++ app/src/main/java/com/msg/gcms/data/remote/dto/datasource/base/BaseRequest.kt
@@ -3,7 +3,7 @@ package com.msg.gcms.data.remote.dto.datasource.base
 import com.google.gson.annotations.SerializedName
 
 data class BaseRequest(
-    @SerializedName("code")
+    @SerializedName("status")
     val statusCode: Int,
     @SerializedName("message")
     val errorMsg: String
