f932278f65559636d32ba0034af91ecfa4db0e2a
김현승 <80810303+khs3994@users.noreply.github.com>
2022-04-04 17:25:34 +0900

    📝 :: 이메일 인증 로직

--- app/src/main/java/com/msg/viewmodel/RegistrationViewModel.kt
+++ app/src/main/java/com/msg/viewmodel/RegistrationViewModel.kt
@@ -5,11 +5,25 @@ import com.msg.gcms.ui.base.BaseViewModel
 import kotlinx.coroutines.launch
 
 class RegistrationViewModel constructor() : BaseViewModel() {
-    fun registrationLogic(email: String, password: String) {
-        val queries: HashMap<String, String> = HashMap()
 
+    private var userEmail: String = ""
+
+    fun registrationLogic(email: String, password: String) {
+        userEmail = email
+
+        val queries: HashMap<String, String> = HashMap()
         queries["email"] = email
         queries["password"] = password
+
+        viewModelScope.launch {
+
+        }
+    }
+
+    fun emailCheckLogic(code: String) {
+        val queries: HashMap<String, String> = HashMap()
+        queries["email"] = userEmail
+        queries["code"] = code
 
         viewModelScope.launch {
 
