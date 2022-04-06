c282c685247a0e77afff22bb611d0ba2dc52f07a
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-04-06 22:33:23 +0900

    동아리 개설 페이지 설정

--- app/src/main/java/com/msg/gcms/ui/component/clubmaker/clubDetail/MakeClubDetailFragment.kt
+++ app/src/main/java/com/msg/gcms/ui/component/clubmaker/clubDetail/MakeClubDetailFragment.kt
@@ -1,5 +1,7 @@
 package com.msg.gcms.ui.component.clubmaker.clubDetail
 
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.msg.gcms.R
 import com.msg.gcms.databinding.FragmentMakeClubDetailBinding
 import com.msg.gcms.ui.base.BaseFragment
@@ -7,5 +9,10 @@ import com.msg.gcms.ui.base.BaseFragment
 class MakeClubDetailFragment :
     BaseFragment<FragmentMakeClubDetailBinding>(R.layout.fragment_make_club_detail) {
     override fun init() {
+
+    }
+
+    fun clickedNextBtn(view : View) {
+        this.findNavController()
     }
 }
--- app/src/main/java/com/msg/gcms/ui/component/clubmaker/clubIntroduce/ClubIntroduceFragment.kt
+++ app/src/main/java/com/msg/gcms/ui/component/clubmaker/clubIntroduce/ClubIntroduceFragment.kt
@@ -1,10 +1,17 @@
 package com.msg.gcms.ui.component.clubmaker.clubIntroduce
 
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.msg.gcms.R
 import com.msg.gcms.databinding.FragmentClubIntroduceBinding
 import com.msg.gcms.ui.base.BaseFragment
 
 class ClubIntroduceFragment : BaseFragment<FragmentClubIntroduceBinding>(R.layout.fragment_club_introduce) {
     override fun init() {
+        binding.fragment = this
+    }
+
+    fun clickedNextButton(view : View) {
+        this.findNavController().navigate(R.id.action_clubIntroduceFragment_to_makeClubDetailFragment)
     }
 }
