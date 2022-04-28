89b91dd7c85fc60fb1c6befd8df63e1a57270438
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-04-28 22:26:39 +0900

    📝 :: add LoginApi

--- app/src/main/java/com/msg/gcms/data/remote/network/CommonAPI.kt
+++ app/src/main/java/com/msg/gcms/data/remote/network/CommonAPI.kt
@@ -1,30 +1,34 @@
 package com.msg.gcms.data.remote.network
 
 import com.msg.gcms.data.remote.dto.datasource.auth.request.CodeIssuanceRequest
+import com.msg.gcms.data.remote.dto.datasource.auth.request.LoginRequest
 import com.msg.gcms.data.remote.dto.datasource.auth.request.QueryString
 import com.msg.gcms.data.remote.dto.datasource.auth.request.RegisterRequest
 import com.msg.gcms.data.remote.dto.datasource.base.BaseResponse
 import com.msg.gcms.di.module.NetworkModule
 import dagger.Component
-import io.reactivex.rxjava3.core.Single
-import retrofit2.Response
 import retrofit2.http.*
 
 interface CommonAPI {
 
     @POST("auth/register")
-    suspend fun postRegistration(
+    suspend fun postSignUp(
         @Body body : RegisterRequest
-    ) : Response<Void>
+    ) : BaseResponse
+
 
     @POST("auth/verify")
     suspend fun postEmail(
          @Body body : CodeIssuanceRequest
-    ) : Response<Void>
+    ) : BaseResponse
 
     @HEAD("auth/verify")
     suspend fun headCheckCode(
-        @Query("email") email : String,
-        @Query("code") code : String
-    ) : Response<Void>
+        @Field("QueryString") QueryString : QueryString
+    ) : BaseResponse
+
+    @POST("auth/login")
+    suspend fun postLogin(
+        @Body body: LoginRequest
+    ) : BaseResponse
 }
