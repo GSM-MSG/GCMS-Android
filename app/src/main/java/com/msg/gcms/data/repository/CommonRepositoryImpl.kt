bb45eee7fb9c521a80908ce412a15281d1c07c77
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-04-29 10:02:16 +0900

    📝 :: add loginRepository

--- app/src/main/java/com/msg/gcms/data/repository/CommonRepositoryImpl.kt
+++ app/src/main/java/com/msg/gcms/data/repository/CommonRepositoryImpl.kt
@@ -2,8 +2,10 @@ package com.msg.gcms.data.repository
 
 import com.msg.gcms.data.remote.datasource.CommonDataSourceImpl
 import com.msg.gcms.data.remote.dto.datasource.auth.request.CodeIssuanceRequest
+import com.msg.gcms.data.remote.dto.datasource.auth.request.LoginRequest
 import com.msg.gcms.data.remote.dto.datasource.auth.request.QueryString
 import com.msg.gcms.data.remote.dto.datasource.auth.request.RegisterRequest
+import com.msg.gcms.data.remote.dto.datasource.auth.response.LoginResponse
 import com.msg.gcms.data.remote.dto.datasource.base.BaseResponse
 import com.msg.gcms.domain.repository.CommonRepository
 import retrofit2.Response
@@ -22,5 +24,9 @@ class CommonRepositoryImpl @Inject constructor(
 
     override suspend fun headCheckCode(email : String, code : String): Response<Void> {
         return datasource.headCheckCode(email = email, code = code)
+    }
+
+    override suspend fun postLogin(body: LoginRequest): Response<LoginResponse> {
+        return datasource.postLogin(body = body)
     }
 }
