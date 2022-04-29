66c9d2fd25f449237e1cb56c53b5e0b5c6141f55
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-04-29 10:02:00 +0900

    📝 :: add loginApi

--- app/src/main/java/com/msg/gcms/data/remote/network/CommonAPI.kt
+++ app/src/main/java/com/msg/gcms/data/remote/network/CommonAPI.kt
@@ -1,8 +1,10 @@
 package com.msg.gcms.data.remote.network
 
 import com.msg.gcms.data.remote.dto.datasource.auth.request.CodeIssuanceRequest
+import com.msg.gcms.data.remote.dto.datasource.auth.request.LoginRequest
 import com.msg.gcms.data.remote.dto.datasource.auth.request.QueryString
 import com.msg.gcms.data.remote.dto.datasource.auth.request.RegisterRequest
+import com.msg.gcms.data.remote.dto.datasource.auth.response.LoginResponse
 import com.msg.gcms.data.remote.dto.datasource.base.BaseResponse
 import com.msg.gcms.di.module.NetworkModule
 import dagger.Component
@@ -27,4 +29,9 @@ interface CommonAPI {
         @Query("email") email : String,
         @Query("code") code : String
     ) : Response<Void>
+
+    @GET("auth/login")
+    suspend fun postLogin(
+        @Body body: LoginRequest
+    ) : Response<LoginResponse>
 }
