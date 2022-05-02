076c86bf01e3f3d0d17501b6f591dd3b7e94b2d5
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-05-03 08:19:57 +0900

    📝 :: LoginToken

---
+++ app/src/main/java/com/msg/gcms/data/local/dao/LoginToken.kt
@@ -0,0 +1,21 @@
+package com.msg.gcms.data.local.dao
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+
+class LoginToken(private val context: Context) {
+    private val name = "accessToken"
+
+    private val prefs = context.getSharedPreferences(name, MODE_PRIVATE)
+
+    var accessToken: String?
+        get() = prefs.getString("accessToken", null)
+        set(value){
+            prefs.edit().putString("accessToken", value).apply()
+        }
+    var refreshToken: String?
+        get() = prefs.getString("refreshToken", null)
+        set(value){
+            prefs.edit().putString("refreshToken", value).apply()
+        }
+}
