1a3fc2e1980fdc7d15c6e511973382c32dab8629
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-05-20 09:41:19 +0900

    ⚡️ :: set banner photos from gallery image

--- app/src/main/java/com/msg/gcms/data/remote/network/ImageAPI.kt
+++ app/src/main/java/com/msg/gcms/data/remote/network/ImageAPI.kt
@@ -5,11 +5,12 @@ import okhttp3.MultipartBody
 import retrofit2.Response
 import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface ImageAPI {
     @Multipart
     @POST("image")
     suspend fun postImage(
-        image : List<MultipartBody.Part>
+        @Part image : List<MultipartBody.Part>
     ): Response<ImageResponse>
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -22,6 +22,7 @@ object Versions {
     const val LIFECYCLE_KTX = "2.3.1"
     const val ROOM = "2.3.0"
     const val LEGACY = "1.0.0"
+    const val RECYCLERVIEW = "1.1.0"
 
     const val HILT = "2.38.1"
     const val MATERIAL = "1.3.0"
