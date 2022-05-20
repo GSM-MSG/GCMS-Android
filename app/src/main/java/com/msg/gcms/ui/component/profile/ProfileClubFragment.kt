e78c1d3652a7b164696960b986968aa17912a8e9
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-05-20 10:39:42 +0900

    📑 :: profileClubFragment

---
+++ app/src/main/java/com/msg/gcms/ui/component/profile/ProfileClubFragment.kt
@@ -0,0 +1,11 @@
+package com.msg.gcms.ui.component.profile
+
+import com.msg.gcms.R
+import com.msg.gcms.databinding.FragmentProfileClubBinding
+import com.msg.gcms.ui.base.BaseFragment
+
+class ProfileClubFragment: BaseFragment<FragmentProfileClubBinding>(R.layout.fragment_profile_club) {
+    override fun init() {
+
+    }
+}
