c3747d1758727dab24be82c3eb50bc147b695338
오종진 <jongjin024@gmail.com>
2022-05-30 23:08:51 +0900

    ✨ :: 방과후 페이지 버튼

---
+++ app/src/main/java/com/msg/gcms/ui/component/afterschool/JoinAfterSchoolActivity.kt
@@ -0,0 +1,12 @@
+package com.msg.gcms.ui.component.afterschool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.msg.gcms.R
+
+class JoinAfterSchoolActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_after_school)
+    }
+}
