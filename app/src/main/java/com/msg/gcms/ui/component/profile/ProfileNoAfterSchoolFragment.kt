5ea99fce2e584f2d51cbce48353100632a8df659
baegteun <74440939+baekteun@users.noreply.github.com>
2022-05-31 21:14:41 +0900

    Revert "Revert "🔀 :: (#140) mypage v2.0""

---
+++ app/src/main/java/com/msg/gcms/ui/component/profile/ProfileNoAfterSchoolFragment.kt
@@ -0,0 +1,11 @@
+package com.msg.gcms.ui.component.profile
+
+import com.msg.gcms.R
+import com.msg.gcms.databinding.FragmentProfileNoAfterSchoolBinding
+import com.msg.gcms.ui.base.BaseFragment
+
+class ProfileNoAfterSchoolFragment: BaseFragment<FragmentProfileNoAfterSchoolBinding>(R.layout.fragment_profile_no_after_school) {
+    override fun init() {
+    }
+
+}
