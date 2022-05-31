b69436b5dae8a385d00eefd2dfd5c2f7037e3eff
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-05-31 20:42:21 +0900

    ⚡️ :: is AfterSchool

--- app/src/main/java/com/msg/gcms/ui/component/profile/ProfileClubFragment.kt
+++ app/src/main/java/com/msg/gcms/ui/component/profile/ProfileClubFragment.kt
@@ -1,11 +1,22 @@
 package com.msg.gcms.ui.component.profile
 
+import androidx.fragment.app.activityViewModels
 import com.msg.gcms.R
 import com.msg.gcms.databinding.FragmentProfileClubBinding
 import com.msg.gcms.ui.base.BaseFragment
+import com.msg.viewmodel.ProfileViewModel
 
 class ProfileClubFragment: BaseFragment<FragmentProfileClubBinding>(R.layout.fragment_profile_club) {
+    private val viewModel by activityViewModels<ProfileViewModel>()
     override fun init() {
+        isAfterSchool()
+    }
 
+    private fun isAfterSchool() {
+        viewModel.afterSchoolStatus.observe(this){
+            if(it){
+                childFragmentManager.beginTransaction().replace(R.id.afterSchoolFragmentView, ProfileAfterSchoolFragment()).commit()
+            }
+        }
     }
 }
