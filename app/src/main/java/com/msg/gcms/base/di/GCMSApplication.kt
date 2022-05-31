f8b9a1d6860caf7a79a582bf49b73c8f7b7994df
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-05-31 22:33:26 +0900

    ⚰️ :: LoginToken

--- app/src/main/java/com/msg/gcms/base/di/GCMSApplication.kt
+++ app/src/main/java/com/msg/gcms/base/di/GCMSApplication.kt
@@ -1,17 +1,9 @@
 package com.msg.gcms.base.di
 
 import android.app.Application
-import com.msg.gcms.data.local.dao.LoginAccessToken
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GCMSApplication : Application() {
-    companion object {
-        lateinit var prefs: LoginAccessToken
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        prefs = LoginAccessToken(applicationContext)
-    }
+    
 }
