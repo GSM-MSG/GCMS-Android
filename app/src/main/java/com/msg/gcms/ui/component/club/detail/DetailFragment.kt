394f0786ecc6c00c8759ae32067b2deeff323c5d
김현승 <80810303+khs3994@users.noreply.github.com>
2022-06-02 08:49:16 +0900

    🔥 :: 이상한 로직 삭제

--- app/src/main/java/com/msg/gcms/ui/component/club/detail/DetailFragment.kt
+++ app/src/main/java/com/msg/gcms/ui/component/club/detail/DetailFragment.kt
@@ -12,57 +12,13 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailFragment : BaseFragment<FragmentClubDetailBinding>(R.layout.fragment_club_detail) {
-
-    private val viewModel by activityViewModels<ClubDetailViewModel>()
-
     override fun init() {
         clickBackBtn()
-        initResult()
-        checkRole()
     }
 
     private fun clickBackBtn() {
         binding.backBtn.setOnClickListener {
             findNavController().popBackStack()
-        }
-    }
-
-    private fun initResult() {
-        viewModel.result.let {
-            //club
-            binding.clubName.text = it.body()!!.club.title
-            binding.explainClubTxt.text = it.body()!!.club.description
-            binding.link.text = it.body()!!.club.relatedLink[1].toString()
-            //boss
-            binding.bossImg.load(it.body()!!.head.userImg)
-            binding.boss.text = it.body()!!.head.name
-            //teacher
-            binding.teacher.text = it.body()!!.club.teacher
-            //contact
-            binding.directoryTxt.text = it.body()!!.club.contact
-            //banner
-            binding.clubBanner.load(it.body()!!.club.bannerUrl)
-        }
-    }
-
-    private fun checkRole() {
-        when (viewModel.result.body()!!.scope) {
-            "HEAD" -> binding.submitBtn.setBackgroundResource(R.drawable.ic_close_application_btn)
-            "USER" -> {
-                if (viewModel.result.body()!!.isApplied) {
-                    binding.submitBtn.setBackgroundResource(R.drawable.ic_application_cancel_btn)
-                } else if (!viewModel.result.body()!!.club.isOpened) {
-                    binding.submitBtn.setBackgroundResource(R.drawable.ic_club_application_closed)
-                    binding.submitBtn.isEnabled = false
-                }
-            }
-            "MEMBER" -> {
-                if(!viewModel.result.body()!!.club.isOpened){
-                    binding.submitBtn.setBackgroundResource(R.drawable.ic_club_application_closed)
-                } else {
-                    binding.submitBtn.isGone
-                }
-            }
         }
     }
 }
