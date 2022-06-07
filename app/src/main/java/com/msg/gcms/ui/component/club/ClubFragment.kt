7164e9e669efab0c6bc77b6eb4e358df6ebba74a
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-06-07 17:12:24 +0900

    ⚡️ :: move detailClub

--- app/src/main/java/com/msg/gcms/ui/component/club/ClubFragment.kt
+++ app/src/main/java/com/msg/gcms/ui/component/club/ClubFragment.kt
@@ -1,21 +1,24 @@
 package com.msg.gcms.ui.component.club
 
 import android.content.Intent
-import android.util.Log
+import android.os.Bundle
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.msg.gcms.R
 import com.msg.gcms.databinding.FragmentClubBinding
 import com.msg.gcms.ui.adapter.ClubListAdapter
 import com.msg.gcms.ui.base.BaseFragment
+import com.msg.gcms.ui.component.club.detail.DetailFragment
 import com.msg.gcms.ui.component.clubmaker.MakeClubActivity
 import com.msg.gcms.ui.component.profile.ProfileActivity
+import com.msg.viewmodel.ClubDetailViewModel
 import com.msg.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ClubFragment : BaseFragment<FragmentClubBinding>(R.layout.fragment_club) {
     private val viewModel by activityViewModels<MainViewModel>()
+    private val detailViewModel by activityViewModels<ClubDetailViewModel>()
     private lateinit var adapter: ClubListAdapter
     override fun init() {
         viewModel.getClubList()
@@ -29,6 +32,12 @@ class ClubFragment : BaseFragment<FragmentClubBinding>(R.layout.fragment_club) {
         binding.clubRecyclerView.layoutManager = GridLayoutManager(context, 2)
         viewModel.clubData.observe(this) {
             adapter = ClubListAdapter(viewModel.clubData.value)
+            adapter.setItemOnClickListener(object : ClubListAdapter.OnItemClickListener {
+                override fun onClick(position: Int) {
+                    detailViewModel.setDetailInfo(viewModel.clubData.value?.get(position)!!.type, viewModel.clubData.value?.get(position)!!.title)
+                    parentFragmentManager.beginTransaction().replace(R.id.fragment_club, DetailFragment()).commit()
+                }
+            })
             binding.clubRecyclerView.adapter = adapter
         }
     }
