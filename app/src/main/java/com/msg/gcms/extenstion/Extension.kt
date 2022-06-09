133bcf9564ea4878d117c66683dabf75cc41ec3c
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-06-09 23:19:42 +0900

    Extension 변경

--- app/src/main/java/com/msg/gcms/extenstion/Extension.kt
+++ app/src/main/java/com/msg/gcms/extenstion/Extension.kt
@@ -3,44 +3,55 @@ package com.msg.gcms.extenstion
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
-import android.widget.TextView
-import com.jakewharton.rxbinding4.InitialValueObservable
-import io.reactivex.rxjava3.android.MainThreadDisposable
-import io.reactivex.rxjava3.core.Observer
 
-fun TextView.textChanges(): InitialValueObservable<CharSequence> {
-    return TextviewTextChangesObservable(this)
-}
 
-private class TextviewTextChangesObservable(
-    private val view: TextView
-) : InitialValueObservable<CharSequence>() {
-    override fun subscribeListener(observer: Observer<in CharSequence>) {
-        val listener = Listener(view, observer)
-        observer.onSubscribe(listener)
-        view.addTextChangedListener(listener)
-    }
-    override val initialValue get() = view.text
-
-    private class Listener(
-        private val view: TextView,
-        private val observer: Observer<in CharSequence>
-    ) : MainThreadDisposable(), TextWatcher {
-        override fun onDispose() {
-            view.removeTextChangedListener(this)
-        }
-
+fun EditText.onTextChanged(block: (s: CharSequence?, start: Int, before: Int, count: Int) -> Unit) {
+    addTextChangedListener(object : TextWatcher {
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
         }
 
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            if(!isDisposed) {
-                observer.onNext(p0)
-            }
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            block(s, start, before, count)
         }
 
         override fun afterTextChanged(p0: Editable?) {
         }
-    }
+    })
 }
+
+// fun TextView.textChanges(): InitialValueObservable<CharSequence> {
+//     return TextviewTextChangesObservable(this)
+// }
+//
+// private class TextviewTextChangesObservable(
+//     private val view: TextView
+// ) : InitialValueObservable<CharSequence>() {
+//     override fun subscribeListener(observer: Observer<in CharSequence>) {
+//         val listener = Listener(view, observer)
+//         observer.onSubscribe(listener)
+//         view.addTextChangedListener(listener)
+//     }
+//     override val initialValue get() = view.text
+//
+//     private class Listener(
+//         private val view: TextView,
+//         private val observer: Observer<in CharSequence>
+//     ) : MainThreadDisposable(), TextWatcher {
+//         override fun onDispose() {
+//             view.removeTextChangedListener(this)
+//         }
+//
+//         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//         }
+//
+//         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//             if(!isDisposed) {
+//                 observer.onNext(p0)
+//             }
+//         }
+//
+//         override fun afterTextChanged(p0: Editable?) {
+//         }
+//     }
+// }
 
