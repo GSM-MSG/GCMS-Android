372b5fbe220cb8718b71a0fae940ed4d95a7364e
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-06-17 12:30:02 +0900

    ⚡️ ️:: 바디 내용 LiveData 저장

--- app/src/main/java/com/msg/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/msg/viewmodel/UserViewModel.kt
@@ -17,16 +17,25 @@ class UserViewModel @Inject constructor(
 ) : ViewModel() {
     var clubType = "MAJOR"
 
-    private val _result = MutableLiveData<UserData>()
-    val result : LiveData<UserData> get() = _result
+    private val _result = MutableLiveData<List<UserData>>()
+    val result : LiveData<List<UserData>> get() = _result
 
     fun getSearchUser(queryString: String) {
+        val queryString : HashMap<String,String> = HashMap()
+        queryString["name"] = queryString.toString()
+        queryString["type"] = clubType
         viewModelScope.launch {
-            val queryString : HashMap<String,String> = HashMap()
-            queryString["name"] = queryString.toString()
-            queryString["type"] = clubType
             val response = userCase.getSearchUser(queryString)
             Log.d("TAG", "getSearchUser: $response")
+            when(response.code()){
+               200 -> {
+                   _result.value = response.body()
+                   Log.d("TAG", "getSearchUser: ${result.value}")
+               }
+              else -> {
+
+              }
+            }
         }
     }
 }
