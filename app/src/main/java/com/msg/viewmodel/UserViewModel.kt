8e4fb67dffe5f7f3bd8c73f4adabc27a04bfaad9
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-06-17 13:49:49 +0900

    🐛 ️:: 이상한 값 보내는거 변경

--- app/src/main/java/com/msg/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/msg/viewmodel/UserViewModel.kt
@@ -20,15 +20,17 @@ class UserViewModel @Inject constructor(
     private val _result = MutableLiveData<List<UserData>>()
     val result : LiveData<List<UserData>> get() = _result
 
-    fun getSearchUser(queryString: String) {
+    fun getSearchUser(name: String) {
         val queryString : HashMap<String,String> = HashMap()
-        queryString["name"] = queryString.toString()
+        Log.d("TAG", "getSearchUser: ${queryString.toString()}")
+        queryString["name"] = name
         queryString["type"] = clubType
         viewModelScope.launch {
             val response = userCase.getSearchUser(queryString)
             Log.d("TAG", "getSearchUser: $response")
             when(response.code()){
                200 -> {
+                   Log.d("TAG", "getSearchUser: ${response.body()} ")
                    _result.value = response.body()
                    Log.d("TAG", "getSearchUser: ${result.value}")
                }
