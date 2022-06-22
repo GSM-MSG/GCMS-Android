890a1c3a65bc381f7622697c68c0f279697cd348
김현승 <80810303+khs3994@users.noreply.github.com>
2022-06-22 15:32:12 +0900

    :sparkles: :: 로그인이 되있을시 인트로 페이지 스킵 기능 추가

--- app/src/main/java/com/msg/gcms/ui/component/splash/SplashActivity.kt
+++ app/src/main/java/com/msg/gcms/ui/component/splash/SplashActivity.kt
@@ -1,14 +1,41 @@
 package com.msg.gcms.ui.component.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.msg.gcms.R
+import com.msg.gcms.ui.component.intro.IntroActivity
+import com.msg.gcms.ui.component.main.MainActivity
+import com.msg.viewmodel.RegistrationViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<RegistrationViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        isLogin()
+    }
+
+    private fun isLogin() {
+        viewModel.apply {
+            checkLogin()
+            isLogin.observe(this@SplashActivity) {
+                if (it) {
+                    Handler().postDelayed({
+                        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    }, 2000)
+                } else {
+                    Handler().postDelayed({
+                        startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+                    }, 2000)
+                }
+            }
+        }
     }
 }
