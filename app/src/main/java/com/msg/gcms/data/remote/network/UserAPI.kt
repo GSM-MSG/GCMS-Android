5565bf8fcc083dec8bb91e34d2a794bf5e9708e0
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-06-26 01:37:58 +0900

    📝 :: add body anotation

--- app/src/main/java/com/msg/gcms/data/remote/network/UserAPI.kt
+++ app/src/main/java/com/msg/gcms/data/remote/network/UserAPI.kt
@@ -5,6 +5,7 @@ import com.msg.gcms.data.remote.dto.datasource.user.request.UserProfileRequest
 import com.msg.gcms.data.remote.dto.datasource.user.response.UserData
 import com.msg.gcms.data.remote.dto.datasource.user.response.UserInfoResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PUT
@@ -16,7 +17,7 @@ interface UserAPI {
 
     @PUT("user/profile")
     suspend fun putProfile(
-        body: UserProfileRequest
+        @Body body: UserProfileRequest
     ): Response<Void>
 
     @GET("user/search")
