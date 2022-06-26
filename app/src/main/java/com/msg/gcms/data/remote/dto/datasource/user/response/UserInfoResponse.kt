8c51130384701daadb1dbcee6165afe60a03ab4d
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-06-26 14:16:14 +0900

    📝 :: afters -> afterSchools

--- app/src/main/java/com/msg/gcms/data/remote/dto/datasource/user/response/UserInfoResponse.kt
+++ app/src/main/java/com/msg/gcms/data/remote/dto/datasource/user/response/UserInfoResponse.kt
@@ -3,6 +3,6 @@ package com.msg.gcms.data.remote.dto.datasource.user.response
 data class UserInfoResponse (
     val userData: UserData,
     val clubs: List<ClubData>,
-    val afters: List<AftersData>
+    val afterSchools: List<AftersData>
 )
 
