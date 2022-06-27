50472d3c17b5da186295d9788067951529712b85
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-06-27 21:09:33 +0900

    ♻️ :: delete log

--- app/src/main/java/com/msg/gcms/ui/adapter/AfterSchoolListAdapter.kt
+++ app/src/main/java/com/msg/gcms/ui/adapter/AfterSchoolListAdapter.kt
@@ -16,7 +16,6 @@ class AfterSchoolListAdapter(private val itemList: List<AftersData>): RecyclerVi
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        Log.d("안ㄴ", "onCreateViewHolder: ${itemList}")
         val binding = ListAfterSchoolBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         val viewHolder = AfterSchoolListAdapter.ViewHolder(binding)
         return viewHolder
