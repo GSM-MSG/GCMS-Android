fb5500fdb54fa8091fe263d99f8042c8bfa365b4
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-01-23 17:26:49 +0900

    📄 :: GetClubListUseCase 생성

---
+++ app/src/main/java/com/msg/gcms/domain/usecase/club/GetClubListUseCase.kt
@@ -0,0 +1,10 @@
+package com.msg.gcms.domain.usecase.club
+
+import com.msg.gcms.domain.repository.ClubRepository
+import javax.inject.Inject
+
+class GetClubListUseCase @Inject constructor(
+    private val repository: ClubRepository
+) {
+    suspend operator fun invoke(type: String) = repository.getClubList(type)
+}
