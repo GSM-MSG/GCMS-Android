550c9d943953748af090e6d951350da92d14db77
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-01-23 17:47:30 +0900

    ♻️ :: PutClubCloseUseCase 적용

--- app/src/main/java/com/msg/viewmodel/ClubViewModel.kt
+++ app/src/main/java/com/msg/viewmodel/ClubViewModel.kt
@@ -12,6 +12,7 @@ import com.msg.gcms.domain.usecase.club.ClubDeleteUseCase
 import com.msg.gcms.domain.usecase.club.ClubUseCase
 import com.msg.gcms.domain.usecase.club.PostClubApplyUseCase
 import com.msg.gcms.domain.usecase.club.PostClubCancelUseCase
+import com.msg.gcms.domain.usecase.club.PutClubCloseUseCase
 import com.msg.gcms.domain.usecase.club.PutClubOpenUseCase
 import com.msg.gcms.domain.usecase.user.ExitUseCase
 import com.msg.gcms.ui.base.LottieFragment
@@ -25,6 +26,7 @@ class ClubViewModel @Inject constructor(
     private val postClubApplyUseCase: PostClubApplyUseCase,
     private val postClubCancelUseCase: PostClubCancelUseCase,
     private val putClubOpenUseCase: PutClubOpenUseCase,
+    private val putClubCloseUseCase: PutClubCloseUseCase,
     private val exitUseCase: ExitUseCase,
     private val clubDeleteUseCase: ClubDeleteUseCase,
 ) : ViewModel() {
@@ -79,7 +81,7 @@ class ClubViewModel @Inject constructor(
         viewModelScope.launch {
             try {
                 val response =
-                    clubUseCase.putClubClose(ClubIdentificationRequest(type = type, q = q))
+                    putClubCloseUseCase(ClubIdentificationRequest(type = type, q = q))
                 _getClubStatus.value = response.code()
             } catch (e: Exception) {
                 Log.d(TAG, "error : $e")
