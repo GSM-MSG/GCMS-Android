be53d0caa85ef877783d15cef1d0825f1b2ce9db
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-01-23 17:38:06 +0900

    📄 ::PostClubApplyUseCase 생성

---
+++ app/src/main/java/com/msg/gcms/domain/usecase/club/PostClubApplyUseCase.kt
@@ -0,0 +1,12 @@
+package com.msg.gcms.domain.usecase.club
+
+import com.msg.gcms.data.remote.dto.datasource.club.request.ClubIdentificationRequest
+import com.msg.gcms.domain.repository.ClubRepository
+import javax.inject.Inject
+
+class PostClubApplyUseCase @Inject constructor(
+    private val repository: ClubRepository
+) {
+    suspend operator fun invoke(body: ClubIdentificationRequest) = repository.postClubApply(body)
+
+}
