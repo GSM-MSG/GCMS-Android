6e92f136216752144d6c017b8bb35a52287afa7d
khs3994 <z01039948003@gmail.com>
2023-01-27 16:33:02 +0900

    :memo: :: ci를 위해 Secret Defalut Value추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,19 +23,19 @@ android {
             "String",
             "CLIENT_ID",
             gradleLocalProperties(rootDir)
-                .getProperty("CLIENT_ID", "")
+                .getProperty("CLIENT_ID", "Defalut")
         )
         buildConfigField(
             "String",
             "CLIENT_SECRET",
             gradleLocalProperties(rootDir)
-                .getProperty("CLIENT_SECRET", "")
+                .getProperty("CLIENT_SECRET", "Defalut")
         )
         buildConfigField(
             "String",
             "REDIRECT_URI",
             gradleLocalProperties(rootDir)
-                .getProperty("REDIRECT_URI", "")
+                .getProperty("REDIRECT_URI", "Defalut")
         )
     }
 
