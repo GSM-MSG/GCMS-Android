b743a0bccac563b35ce8610aaf1fc41f9f59c90d
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-01-28 01:51:52 +0900

    ✨️ :: 동아리 리스트 받아오는 로직 예외처기

--- app/src/main/java/com/msg/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/msg/viewmodel/MainViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.msg.gcms.data.remote.dto.datasource.club.response.SummaryClubResponse
+import com.msg.gcms.domain.exception.BadRequestException
+import com.msg.gcms.domain.exception.UnauthorizedException
 import com.msg.gcms.domain.usecase.club.GetClubListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -33,15 +35,22 @@ class MainViewModel @Inject constructor(
     fun getClubList() {
         viewModelScope.launch {
             try {
-                val response = getClubListUseCase(
+                getClubListUseCase(
                     when (clubName.value) {
                         "전공동아리" -> "MAJOR"
                         "자율동아리" -> "FREEDOM"
                         "사설동아리" -> "EDITORIAL"
                         else -> "MAJOR"
                     }
-                )
-                _clubData.value = response.body()
+                ).onSuccess {
+                    _clubData.value = it.body()
+                }.onFailure {
+                    when (it) {
+                        is BadRequestException -> Log.d("TAG", "getClubList: $it")
+                        is UnauthorizedException -> Log.d("TAG", "getClubList: $it")
+                        else -> Log.d("TAG", "getClubList: $it")
+                    }
+                }
             } catch (e: Exception) {
                 Log.d("ERROR", "getClubList: ${e.message}")
             }
