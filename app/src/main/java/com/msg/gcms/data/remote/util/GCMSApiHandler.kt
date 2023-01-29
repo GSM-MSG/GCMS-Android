38aaa22d4205fc1bfc2a3112151ff6ed431eae91
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-01-29 17:05:27 +0900

    ✨️ :: Import 수정

--- app/src/main/java/com/msg/gcms/data/remote/util/GCMSApiHandler.kt
+++ app/src/main/java/com/msg/gcms/data/remote/util/GCMSApiHandler.kt
@@ -1,59 +1,52 @@
 package com.msg.gcms.data.remote.util
 
-import com.msg.gcms.domain.exception.BadRequestException
-import com.msg.gcms.domain.exception.ConflictException
-import com.msg.gcms.domain.exception.ForBiddenException
-import com.msg.gcms.domain.exception.NoInternetException
-import com.msg.gcms.domain.exception.NotFoundException
-import com.msg.gcms.domain.exception.OtherHttpException
-import com.msg.gcms.domain.exception.ServerException
-import com.msg.gcms.domain.exception.TimeOutException
-import com.msg.gcms.domain.exception.UnKnownException
-import com.msg.gcms.domain.exception.UnauthorizedException
+import com.msg.gcms.domain.exception.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
-suspend inline fun <T> gcmsApiHandler(
-    crossinline function: suspend () -> T
-): T {
-    return try {
-        withContext(Dispatchers.IO) {
-            function.invoke()
+class GCMSApiHandler<T> {
+    suspend inline fun invoke(
+        crossinline function: suspend () -> T
+    ): T {
+        return try {
+            withContext(Dispatchers.IO) {
+                function.invoke()
+            }
+        } catch (e: HttpException) {
+            val message = e.message
+            throw when (e.code()) {
+                400 -> BadRequestException(
+                    message = message
+                )
+                401 -> UnauthorizedException(
+                    message = message,
+                )
+                403 -> ForBiddenException(
+                    message = message,
+                )
+                404 -> NotFoundException(
+                    message = message,
+                )
+                409 -> ConflictException(
+                    message = message,
+                )
+                500, 501, 502, 503 -> ServerException(
+                    message = message,
+                )
+                else -> OtherHttpException(
+                    message = message,
+                    code = e.code()
+                )
+            }
+        } catch (e: SocketTimeoutException) {
+            throw TimeOutException(message = e.message)
+        } catch (e: UnknownHostException) {
+            throw NoInternetException()
+        } catch (e: Exception) {
+            throw UnKnownException(message = e.message)
         }
-    } catch (e: HttpException) {
-        val message = e.message
-        throw when (e.code()) {
-            400 -> BadRequestException(
-                message = message
-            )
-            401 -> UnauthorizedException(
-                message = message,
-            )
-            403 -> ForBiddenException(
-                message = message,
-            )
-            404 -> NotFoundException(
-                message = message,
-            )
-            409 -> ConflictException(
-                message = message,
-            )
-            500, 501, 502, 503 -> ServerException(
-                message = message,
-            )
-            else -> OtherHttpException(
-                message = message,
-                code = e.code()
-            )
-        }
-    } catch (e: SocketTimeoutException) {
-        throw TimeOutException(message = e.message)
-    } catch (e: UnknownHostException) {
-        throw NoInternetException()
-    } catch (e: Exception) {
-        throw UnKnownException(message = e.message)
     }
 }
