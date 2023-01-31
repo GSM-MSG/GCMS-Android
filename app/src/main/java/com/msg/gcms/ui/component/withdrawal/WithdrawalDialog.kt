554661256ac8543864f29d212a0f613abfdb3c0a
khs3994 <z01039948003@gmail.com>
2023-02-01 04:37:22 +0900

    :memo: :: manifest수정 및 불필요한 파일 삭제

---
+++ app/src/main/java/com/msg/gcms/ui/component/withdrawal/WithdrawalDialog.kt
@@ -0,0 +1,48 @@
+package com.msg.gcms.ui.component.withdrawal
+
+import android.content.Context
+import android.os.Bundle
+import android.view.Gravity
+import android.view.WindowManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.msg.gcms.databinding.LogoutDialogBinding
+
+class WithdrawalDialog(context: Context) : BottomSheetDialog(context) {
+    lateinit var withdrawalDialogListener: WithdrawalDialogListener
+    lateinit var binding: LogoutDialogBinding
+
+    interface WithdrawalDialogListener {
+        fun logout()
+        fun goWithdrawal()
+    }
+
+    fun setDialogListener(withdrawalDialogListener: WithdrawalDialogListener) {
+        this.withdrawalDialogListener = withdrawalDialogListener
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = LogoutDialogBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        dialog()
+    }
+
+    fun dialog() = with(binding) {
+        setCanceledOnTouchOutside(true)
+        window!!.apply {
+            setLayout(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT
+            )
+            setGravity(Gravity.BOTTOM)
+        }
+        logoutBtn.setOnClickListener {
+            withdrawalDialogListener.logout()
+            this@WithdrawalDialog.dismiss()
+        }
+        withdrawalBtn.setOnClickListener {
+            withdrawalDialogListener.goWithdrawal()
+            this@WithdrawalDialog.dismiss()
+        }
+    }
+}
