c2062c7349cf71d7a5cfe8b3698978fbb1aacc9a
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-01-31 21:29:29 +0900

    ✨ :: 오류 추가, 설명용 주석 추가

--- app/src/main/java/com/msg/viewmodel/util/Event.kt
+++ app/src/main/java/com/msg/viewmodel/util/Event.kt
@@ -2,12 +2,54 @@ package com.msg.viewmodel.util
 
 sealed class Event (
 ) {
+    /**
+     * 성공
+     */
     object Success: Event()
+
+    /**
+     * 400번 요청이 올바르지 않은 경우
+     */
     object BadRequest: Event()
-    object ForBidden: Event()
-    object NotFound: Event()
-    object UnKnown: Event()
-    object TooMuch: Event()
+
+    /**
+     * 401번 비인증 요청
+     */
     object Unauthorized: Event()
+
+    /**
+     * 403번 권한이 없음
+     */
+    object ForBidden: Event()
+
+    /**
+     * 404 찾을 수 없는 경우
+     */
+    object NotFound: Event()
+
+    /**
+     * 406 맞는 규격이 없는 경우
+     */
+    object  NotAcceptable: Event()
+
+    /**
+     * 408 요청이 너무 오래 걸리는 경우
+     */
+    object TimeOut: Event()
+
+    /**
+     * 409 권한이 없을 때
+     */
+    object Conflict: Event()
+
+    /**
+     * 50X 서버에러
+     */
+    object Server: Event()
+
+    /**
+     * 예상치 못한 에러
+     */
+    object UnKnown: Event()
+
 }
-// object Success: Event()
