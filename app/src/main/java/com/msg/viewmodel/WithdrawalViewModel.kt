05ecc6a43ef5ac75adb803542f4e45301a1e877e
khs3994 <z01039948003@gmail.com>
2023-02-01 18:08:04 +0900

    :memo: :: 지웠던 회원탈퇴 관련 코드들 복구

---
+++ app/src/main/java/com/msg/viewmodel/WithdrawalViewModel.kt
@@ -0,0 +1,28 @@
+package com.msg.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.msg.gcms.domain.usecase.user.DeleteUserUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WithdrawalViewModel @Inject constructor(
+    private val deleteUserUseCase: DeleteUserUseCase
+) : ViewModel() {
+
+    private val _isApproved = MutableLiveData<Boolean>()
+    val isApproved: LiveData<Boolean> get() = _isApproved
+
+    private val _isWithdrawal = MutableLiveData<Boolean>()
+    val isWithdrawal: LiveData<Boolean> get() = _isWithdrawal
+
+    fun changeIsApproved(isCheck: Boolean) {
+        _isApproved.value = isCheck
+    }
+
+    fun withdrawal() {
+        TODO("GAuth에 맞는 방식으로 변경 필요")
+    }
+}
