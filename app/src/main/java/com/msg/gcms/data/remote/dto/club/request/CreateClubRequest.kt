b038795d3c8b522ebd99676fa94b9b18d4dc9943
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-12 07:34:22 +0900

    👽 :: 동아리 생성 로직 request 수정

--- app/src/main/java/com/msg/gcms/data/remote/dto/club/request/CreateClubRequest.kt
+++ app/src/main/java/com/msg/gcms/data/remote/dto/club/request/CreateClubRequest.kt
@@ -1,13 +1,24 @@
 package com.msg.gcms.data.remote.dto.club.request
 
+import com.google.gson.annotations.SerializedName
+
 data class CreateClubRequest(
+    @SerializedName("type")
     val type: String,
+    @SerializedName("name")
     val title: String,
+    @SerializedName("content")
     val description: String,
+    @SerializedName("contact")
     val contact: String,
+    @SerializedName("notionLink")
     val notionLink: String,
+    @SerializedName("teacher")
     val teacher: String?,
+    @SerializedName("member")
     val member: List<String>?,
+    @SerializedName("activityImgs")
     val activityUrls: List<String>?,
+    @SerializedName("bannerImg")
     val bannerUrl:String
 )
