ef4480b8cebb433746e70c0eb37cb784a4ac4bbc
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-12 22:51:38 +0900

    ✨️ :: repository di 추가

--- app/src/main/java/com/msg/gcms/di/module/RepositoryModule.kt
+++ app/src/main/java/com/msg/gcms/di/module/RepositoryModule.kt
@@ -1,15 +1,21 @@
 package com.msg.gcms.di.module
 
-import com.msg.gcms.data.remote.datasource.club_data.ClubDataSourceImpl
+import com.msg.gcms.data.remote.datasource.applicant.ApplicantDataSourceImpl
 import com.msg.gcms.data.remote.datasource.auth.AuthDataSourceImpl
+import com.msg.gcms.data.remote.datasource.club_data.ClubDataSourceImpl
+import com.msg.gcms.data.remote.datasource.club_member.ClubMemberDataSourceImpl
 import com.msg.gcms.data.remote.datasource.image.ImageDataSourceImpl
 import com.msg.gcms.data.remote.datasource.user_data.UserDataSourceImpl
-import com.msg.gcms.data.repository.ClubRepositoryImpl
+import com.msg.gcms.data.repository.ApplicantRepositoryImpl
 import com.msg.gcms.data.repository.AuthRepositoryImpl
+import com.msg.gcms.data.repository.ClubMemberRepositoryImpl
+import com.msg.gcms.data.repository.ClubRepositoryImpl
 import com.msg.gcms.data.repository.ImageRepositoryImpl
 import com.msg.gcms.data.repository.UserRepositoryImpl
-import com.msg.gcms.domain.repository.ClubRepository
+import com.msg.gcms.domain.repository.ApplicantRepository
 import com.msg.gcms.domain.repository.AuthRepository
+import com.msg.gcms.domain.repository.ClubMemberRepository
+import com.msg.gcms.domain.repository.ClubRepository
 import com.msg.gcms.domain.repository.ImageRepository
 import com.msg.gcms.domain.repository.UserRepository
 import dagger.Module
@@ -28,8 +34,18 @@ object RepositoryModule {
 
     @Provides
     @Singleton
+    fun provideApplicantRepository(dataSource: ApplicantDataSourceImpl): ApplicantRepository =
+        ApplicantRepositoryImpl(dataSource)
+
+    @Provides
+    @Singleton
     fun provideClubRepository(dataSource: ClubDataSourceImpl): ClubRepository =
         ClubRepositoryImpl(dataSource)
+
+    @Provides
+    @Singleton
+    fun provideClubMemberRepository(dataSource: ClubMemberDataSourceImpl): ClubMemberRepository =
+        ClubMemberRepositoryImpl(dataSource)
 
     @Provides
     @Singleton
