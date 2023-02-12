c52c658bdf58cc9a9bc18b96cdddbbca1116ccac
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-12 16:02:56 +0900

    👽 :: api 변경에 따른 변경

--- app/src/main/java/com/msg/gcms/data/repository/ClubRepositoryImpl.kt
+++ app/src/main/java/com/msg/gcms/data/repository/ClubRepositoryImpl.kt
@@ -3,10 +3,8 @@ package com.msg.gcms.data.repository
 import com.msg.gcms.data.remote.datasource.club_data.ClubDataSourceImpl
 import com.msg.gcms.data.remote.dto.club.request.ClubIdentificationRequest
 import com.msg.gcms.data.remote.dto.club.request.CreateClubRequest
-import com.msg.gcms.data.remote.dto.club.request.MemberManagementRequest
 import com.msg.gcms.data.remote.dto.club.request.ModifyClubInfoRequest
 import com.msg.gcms.data.remote.dto.club.response.ClubInfoResponse
-import com.msg.gcms.data.remote.dto.club.response.MemberInfo
 import com.msg.gcms.data.remote.dto.club.response.SummaryClubResponse
 import com.msg.gcms.domain.repository.ClubRepository
 import javax.inject.Inject
@@ -34,25 +32,6 @@ class ClubRepositoryImpl @Inject constructor(
         return dataSource.deleteClub(clubId = clubId)
     }
 
-    override suspend fun getMemberList(clubName: String, type: String): MemberInfo {
-        return dataSource.getMemberList(clubName = clubName, type = type)
-    }
-
-    override suspend fun getApplicationList(
-        clubName: String,
-        type: String
-    ): MemberInfo {
-        return dataSource.getApplicantList(clubName = clubName, type = type)
-    }
-
-    override suspend fun postApplicationAccept(body: MemberManagementRequest) {
-        return dataSource.postApplicationAccept(body = body)
-    }
-
-    override suspend fun postApplicationReject(body: MemberManagementRequest) {
-        return dataSource.postApplicationReject(body = body)
-    }
-
     override suspend fun putClubOpen(body: ClubIdentificationRequest) {
         return dataSource.putClubOpen(body = body)
     }
@@ -61,19 +40,8 @@ class ClubRepositoryImpl @Inject constructor(
         return dataSource.putClubClose(body = body)
     }
 
-    override suspend fun deleteMemberExpel(body: MemberManagementRequest) {
-        return dataSource.deleteMemberExpel(body = body)
-    }
-
-    override suspend fun postClubApply(body: ClubIdentificationRequest) {
-        return dataSource.postClubApply(body = body)
-    }
-
     override suspend fun postClubCancel(body: ClubIdentificationRequest) {
         return dataSource.postClubCancel(body = body)
     }
 
-    override suspend fun putDelegationOfRepresentation(body: MemberManagementRequest) {
-        return dataSource.putDelegationOfRepresentation(body = body)
-    }
 }
--- app/src/main/java/com/msg/gcms/domain/repository/ClubRepository.kt
+++ app/src/main/java/com/msg/gcms/domain/repository/ClubRepository.kt
@@ -2,10 +2,8 @@ package com.msg.gcms.domain.repository
 
 import com.msg.gcms.data.remote.dto.club.request.ClubIdentificationRequest
 import com.msg.gcms.data.remote.dto.club.request.CreateClubRequest
-import com.msg.gcms.data.remote.dto.club.request.MemberManagementRequest
 import com.msg.gcms.data.remote.dto.club.request.ModifyClubInfoRequest
 import com.msg.gcms.data.remote.dto.club.response.ClubInfoResponse
-import com.msg.gcms.data.remote.dto.club.response.MemberInfo
 import com.msg.gcms.data.remote.dto.club.response.SummaryClubResponse
 
 interface ClubRepository {
@@ -24,23 +22,9 @@ interface ClubRepository {
 
     suspend fun deleteClub(clubId: Long)
 
-    suspend fun getMemberList(clubName: String, type: String): MemberInfo
-
-    suspend fun getApplicationList(clubName: String, type: String): MemberInfo
-
-    suspend fun postApplicationAccept(body: MemberManagementRequest)
-
-    suspend fun postApplicationReject(body: MemberManagementRequest)
-
     suspend fun putClubOpen(body: ClubIdentificationRequest)
 
     suspend fun putClubClose(body: ClubIdentificationRequest)
 
-    suspend fun deleteMemberExpel(body: MemberManagementRequest)
-
-    suspend fun postClubApply(body: ClubIdentificationRequest)
-
     suspend fun postClubCancel(body: ClubIdentificationRequest)
-
-    suspend fun putDelegationOfRepresentation(body: MemberManagementRequest)
 }
