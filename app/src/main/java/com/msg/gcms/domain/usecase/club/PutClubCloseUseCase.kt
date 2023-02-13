d809fc50fe1a5e7d1466016d75e12a421cf8cb9c
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-13 17:33:25 +0900

    👽️ :: useCase 변경

--- app/src/main/java/com/msg/gcms/domain/usecase/club/PutClubCloseUseCase.kt
+++ app/src/main/java/com/msg/gcms/domain/usecase/club/PutClubCloseUseCase.kt
@@ -1,13 +1,12 @@
 package com.msg.gcms.domain.usecase.club
 
-import com.msg.gcms.data.remote.dto.club.request.ClubIdentificationRequest
 import com.msg.gcms.domain.repository.ClubRepository
 import javax.inject.Inject
 
 class PutClubCloseUseCase @Inject constructor(
     private val repository: ClubRepository
 ){
-    suspend operator fun invoke(body: ClubIdentificationRequest) = kotlin.runCatching {
-        repository.putClubClose(body)
+    suspend operator fun invoke(clubId: Long) = kotlin.runCatching {
+        repository.putClubClose(clubId = clubId)
     }
 }
