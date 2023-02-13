6521bafbe45ecfa60e9d4606874bec47667e50eb
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-13 17:54:43 +0900

    👽 :: retrofit 직렬화 방법 수정

--- app/src/main/java/com/msg/gcms/data/remote/dto/user/response/UserData.kt
+++ app/src/main/java/com/msg/gcms/data/remote/dto/user/response/UserData.kt
@@ -1,13 +1,20 @@
 package com.msg.gcms.data.remote.dto.user.response
 
-import java.io.Serializable
+import com.google.gson.annotations.SerializedName
 
 data class UserData(
+    @SerializedName("uuid")
     val uuid: String,
+    @SerializedName("email")
     val email: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("gradle")
     val grade: Int,
+    @SerializedName("classNum")
     val `class`: Int,
+    @SerializedName("number")
     val num: Int,
+    @SerializedName("profileImg")
     val userImg: String
-): Serializable
+)
