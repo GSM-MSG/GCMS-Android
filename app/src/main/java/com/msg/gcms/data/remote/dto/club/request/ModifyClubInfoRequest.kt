e52e5a915a5decbd31b83c63b1ed77a02b2e3b59
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-14 21:09:50 +0900

    ✨ :: 코드 리뷰 반영

--- app/src/main/java/com/msg/gcms/data/remote/dto/club/request/ModifyClubInfoRequest.kt
+++ app/src/main/java/com/msg/gcms/data/remote/dto/club/request/ModifyClubInfoRequest.kt
@@ -4,21 +4,22 @@ import com.google.gson.annotations.SerializedName
 
 data class ModifyClubInfoRequest(
     @SerializedName("type")
-    val type : String,
-    @SerializedName("type")
-    val title : String,
-    @SerializedName("type")
-    val description : String,
-    @SerializedName("type")
-    val contact : String,
-    @SerializedName("type")
-    val notionLink: String?,
-    @SerializedName("type")
-    val teacher :String?,
-    @SerializedName("type")
-    val deleteActivityUrls: List<String>?,
-    @SerializedName("type")
+    val type: String,
+    @SerializedName("name")
+    val title: String,
+    @SerializedName("content")
+    val description: String,
+    @SerializedName("bannerImg")
     val bannerUrl: String,
-    @SerializedName("type")
-    val newActivityUrls: List<String>?
+    @SerializedName("contact")
+    val contact: String,
+    @SerializedName("notionLink")
+    val notionLink: String?,
+    @SerializedName("teacher")
+    val teacher: String?,
+    @SerializedName("activityImgs")
+    val activityImgs: List<String>,
+    @SerializedName("member")
+    val member: List<String>
+
 )
