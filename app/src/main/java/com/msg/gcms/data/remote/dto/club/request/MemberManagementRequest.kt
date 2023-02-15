d232dd976bc88fd75678ca01726327ffd5324641
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-16 02:01:57 +0900

    ⚰️ :: data class 제거

--- app/src/main/java/com/msg/gcms/data/remote/dto/club/request/MemberManagementRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.msg.gcms.data.remote.dto.club.request
-
-import com.google.gson.annotations.SerializedName
-
-data class MemberManagementRequest(
-    @SerializedName("uuid")
-    val uuid: String
-)
