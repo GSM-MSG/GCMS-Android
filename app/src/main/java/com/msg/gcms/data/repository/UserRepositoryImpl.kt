45d8057b92d1479902f29d2645a8874dd73d6ea9
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-16 17:48:35 +0900

    ✨ :: 계층별 data class 적용

--- app/src/main/java/com/msg/gcms/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/msg/gcms/data/repository/UserRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.msg.gcms.data.repository
 import com.msg.gcms.data.mapper.UserMapper
 import com.msg.gcms.data.remote.datasource.user.UserDataSourceImpl
 import com.msg.gcms.data.remote.dto.user.modify_profile_image.ModifyProfileImageRequest
+import com.msg.gcms.domain.data.user.modify_profile_image.ModifyProfileImageData
 import com.msg.gcms.data.remote.dto.user.response.UserData
 import com.msg.gcms.domain.data.user.get_my_profile.GetMyProfileData
 import com.msg.gcms.domain.repository.UserRepository
@@ -16,12 +17,12 @@ class UserRepositoryImpl @Inject constructor(
     }
 
     override suspend fun putProfile(
-        body: ModifyProfileImageRequest
+        body: ModifyProfileImageData
     ) {
-        return dataSource.putProfile(body)
+        return dataSource.putProfile(ModifyProfileImageRequest(url = body.url))
     }
 
-    override suspend fun getUserSearch(QueryString: Map<String,String>): List<UserData> {
+    override suspend fun getUserSearch(QueryString: Map<String, String>): List<UserData> {
         return dataSource.getUserSearch(QueryString)
     }
 
