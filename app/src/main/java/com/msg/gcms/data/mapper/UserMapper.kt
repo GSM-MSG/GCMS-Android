13e8b50648fe7dd24779b6613d6475974653d10e
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-16 17:03:49 +0900

    ✨️ :: mapper 생성

---
+++ app/src/main/java/com/msg/gcms/data/mapper/UserMapper.kt
@@ -0,0 +1,31 @@
+package com.msg.gcms.data.mapper
+
+import com.msg.gcms.data.remote.dto.user.get_my_profile.GetMyProfileResponse
+import com.msg.gcms.data.remote.dto.user.get_my_profile.ProfileClubResponse
+import com.msg.gcms.domain.data.user.get_my_profile.GetMyProfileData
+import com.msg.gcms.domain.data.user.get_my_profile.ProfileClubData
+
+object UserMapper {
+
+    fun mapperToGetMyProfileData(data: GetMyProfileResponse): GetMyProfileData {
+        return GetMyProfileData(
+            classNum = data.classNum,
+            clubs = data.clubs.map { mapperToProfileClubData(it) },
+            email = data.email,
+            grade = data.grade,
+            name = data.name,
+            number = data.number,
+            profileImg = data.profileImg,
+            uuid = data.uuid
+        )
+    }
+
+    private fun mapperToProfileClubData(data: ProfileClubResponse): ProfileClubData {
+        return ProfileClubData(
+            id = data.id,
+            type = data.type,
+            bannerImg = data.bannerImg,
+            title = data.title
+        )
+    }
+}
