b0596a1127fbeb983c4bfe25a86a4d0b9067fb41
khs3994 <z01039948003@gmail.com>
2023-02-16 18:45:55 +0900

    :sparkles: :: 회원 탈퇴 로직 수정

--- app/src/main/java/com/msg/gcms/presentation/viewmodel/WithdrawalViewModel.kt
+++ app/src/main/java/com/msg/gcms/presentation/viewmodel/WithdrawalViewModel.kt
@@ -3,8 +3,14 @@ package com.msg.gcms.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.msg.gcms.domain.exception.BadRequestException
+import com.msg.gcms.domain.exception.NotFoundException
+import com.msg.gcms.domain.exception.UnauthorizedException
 import com.msg.gcms.domain.usecase.user.DeleteUserUseCase
+import com.msg.gcms.presentation.viewmodel.util.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,14 +21,24 @@ class WithdrawalViewModel @Inject constructor(
     private val _isApproved = MutableLiveData<Boolean>()
     val isApproved: LiveData<Boolean> get() = _isApproved
 
-    private val _isWithdrawal = MutableLiveData<Boolean>()
-    val isWithdrawal: LiveData<Boolean> get() = _isWithdrawal
+    private val _withDrawalRequest = MutableLiveData<Event>()
+    val withDrawalRequest: LiveData<Event> get() = _withDrawalRequest
 
     fun changeIsApproved(isCheck: Boolean) {
         _isApproved.value = isCheck
     }
 
-    fun withdrawal() {
-        TODO("GAuth에 맞는 방식으로 변경 필요")
+    fun withdrawal() = viewModelScope.launch {
+        deleteUserUseCase()
+            .onSuccess {
+                _withDrawalRequest.value = Event.Success
+            }.onFailure {
+                _withDrawalRequest.value = when (it) {
+                    is UnauthorizedException -> Event.Unauthorized
+                    is NotFoundException -> Event.NotFound
+                    is BadRequestException -> Event.BadRequest
+                    else -> Event.UnKnown
+                }
+            }
     }
 }
