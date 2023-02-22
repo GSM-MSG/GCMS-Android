f5b33ecd91d05feb6ffc9053aa303b7e505efdb2
khs3994 <z01039948003@gmail.com>
2023-02-22 16:25:04 +0900

    :memo: :: dataSourceModule Binds로 변경

--- app/src/main/java/com/msg/gcms/di/module/StorageModule.kt
+++ app/src/main/java/com/msg/gcms/di/module/StorageModule.kt
@@ -9,7 +9,7 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class LocalDataStorageModule {
+abstract class StorageModule {
     @Binds
     abstract fun provideAuthDataStorage(
         authDataStorageImpl: AuthDataStorageImpl
