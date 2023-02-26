15f3ab2a2fee730d8d58bacdac51e01ce823a5e4
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-02-26 18:41:24 +0900

    :sparkles: 로그 기능 추가

--- app/src/main/java/com/msg/gcms/presentation/viewmodel/util/ErrorHandling.kt
+++ app/src/main/java/com/msg/gcms/presentation/viewmodel/util/ErrorHandling.kt
@@ -1,5 +1,6 @@
 package com.msg.gcms.presentation.viewmodel.util
 
+import android.util.Log
 import com.msg.gcms.domain.exception.BadRequestException
 import com.msg.gcms.domain.exception.ConflictException
 import com.msg.gcms.domain.exception.ForBiddenException
@@ -23,39 +24,52 @@ suspend fun Throwable.errorHandling(
 ): Event =
     when (this) {
         is BadRequestException -> {
+            errorLog("BadRequestException", message)
             badRequestAction()
             Event.BadRequest
         }
         is UnauthorizedException, is NeedLoginException -> {
+            errorLog("UnauthorizedException", message)
             unauthorizedAction()
             Event.Unauthorized
         }
         is ForBiddenException -> {
+            errorLog("ForBiddenException", message)
             forBiddenAction()
             Event.ForBidden
         }
         is NotFoundException -> {
+            errorLog("NotFoundException", message)
             notFoundAction()
             Event.NotFound
         }
         is NotAcceptableException -> {
+            errorLog("NotAcceptableException", message)
             notAcceptableAction()
             Event.NotAcceptable
         }
         is TimeOutException -> {
+            errorLog("TimeOutException", message)
             timeOutAction()
             Event.TimeOut
         }
         is ConflictException -> {
+            errorLog("ConflictException", message)
             conflictAction()
             Event.Conflict
         }
         is ServerException -> {
+            errorLog("ServerException", message)
             serverAction()
             Event.Server
         }
         else -> {
+            errorLog("UnKnownException", message)
             unknownAction()
             Event.UnKnown
         }
     }
+
+private fun errorLog(tag: String, msg: String?) {
+    Log.d("ErrorHandling-$tag", msg ?: "알 수 없는 오류")
+}
