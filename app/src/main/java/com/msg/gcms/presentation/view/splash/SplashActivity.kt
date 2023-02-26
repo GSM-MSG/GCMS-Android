232daa449ddf8a2cd28bde8a36654a3c94a4413e
khs3994 <z01039948003@gmail.com>
2023-02-26 20:27:37 +0900

    :memo: :: 스플래쉬에서 토큰 정상인지 검사하는 로직 추가

--- app/src/main/java/com/msg/gcms/presentation/view/splash/SplashActivity.kt
+++ app/src/main/java/com/msg/gcms/presentation/view/splash/SplashActivity.kt
@@ -6,10 +6,13 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.msg.gcms.R
+import com.msg.gcms.presentation.base.BaseModal
 import com.msg.gcms.presentation.utils.enterActivity
 import com.msg.gcms.presentation.view.intro.IntroActivity
 import com.msg.gcms.presentation.view.main.MainActivity
+import com.msg.gcms.presentation.viewmodel.MainViewModel
 import com.msg.gcms.presentation.viewmodel.SplashViewModel
+import com.msg.gcms.presentation.viewmodel.util.Event
 import com.msg.gcms.util.VersionChecker
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,6 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     private val splashViewModel by viewModels<SplashViewModel>()
+    private val mainViewModel by viewModels<MainViewModel>()
     private lateinit var versionChecker: VersionChecker
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,13 +36,23 @@ class SplashActivity : AppCompatActivity() {
 
     private fun startActivity() {
         splashViewModel.isLoginAble.observe(this) {
-            enterActivity(
-                this@SplashActivity,
-                if (it)
-                    MainActivity()
-                else
-                    IntroActivity()
-            )
+            if (it) checkTokenIsNormal()
+            else enterActivity(this@SplashActivity, IntroActivity())
+        }
+    }
+
+    private fun checkTokenIsNormal() {
+        mainViewModel.getClubList()
+        observeGetClubEvent()
+    }
+
+    private fun observeGetClubEvent() {
+        mainViewModel.getClubList.observe(this) {
+            when (it) {
+                Event.Success -> enterActivity(this@SplashActivity, MainActivity())
+                Event.Unauthorized -> enterActivity(this@SplashActivity, IntroActivity())
+                else -> BaseModal("오류", "알수 없는 오류가 발생했습니다.", this).show()
+            }
         }
     }
 
