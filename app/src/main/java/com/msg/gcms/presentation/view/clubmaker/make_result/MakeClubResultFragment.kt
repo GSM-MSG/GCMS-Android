5f08cc36d17bc1ecb6dfe630d7e905f26149b42f
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-01 00:21:03 +0900

    ✨ :: 동아리 생성 결과 페이지 생성

---
+++ app/src/main/java/com/msg/gcms/presentation/view/clubmaker/make_result/MakeClubResultFragment.kt
@@ -0,0 +1,11 @@
+package com.msg.gcms.presentation.view.clubmaker.make_result
+
+import com.msg.gcms.R
+import com.msg.gcms.databinding.FragmentMakeClubResultBinding
+import com.msg.gcms.presentation.base.BaseFragment
+
+class MakeClubResultFragment : BaseFragment<FragmentMakeClubResultBinding>(R.layout.fragment_make_club_result) {
+    override fun init() {
+
+    }
+}
