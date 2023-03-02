1db7a5f961539bce18bdcc145d75d5035e455fc7
khs3994 <z01039948003@gmail.com>
2023-03-02 11:14:01 +0900

    :memo: :: 생성 시도후 result페이지로 가도록 변경

--- app/src/main/java/com/msg/gcms/presentation/view/clubmaker/club_detail/MakeClubDetailFragment.kt
+++ app/src/main/java/com/msg/gcms/presentation/view/clubmaker/club_detail/MakeClubDetailFragment.kt
@@ -282,36 +282,6 @@ class MakeClubDetailFragment :
                 Event.Conflict -> {
                     BaseModal("생성 실패", "이미 존재하는 동아리 입니다.", requireContext()).show()
                 }
-                Event.ForBidden -> {
-                    BaseModal(
-                        "생성 실패",
-                        "이미 다른 동아리에 소속되어있습니다.",
-                        requireContext()
-                    ).let { dialog ->
-                        dialog.show()
-                        dialog.dialogBinding.ok.setOnClickListener {
-                            enterActivity(requireActivity(), IntroActivity())
-                            dialog.dismiss()
-                        }
-                    }
-                }
-                Event.Server -> {
-                    BaseModal(
-                        "오류",
-                        "서버상의 오류가 발생했습니다.",
-                        requireContext()
-                    ).let { dialog ->
-                        dialog.show()
-                        dialog.dialogBinding.ok.setOnClickListener {
-                            enterActivity(requireActivity(), IntroActivity())
-                            dialog.dismiss()
-                        }
-                    }
-                }
-                Event.Success -> {
-                    this.findNavController()
-                        .navigate(R.id.action_makeClubDetailFragment_to_makeClubResultFragment)
-                }
                 else -> {
                     this.findNavController()
                         .navigate(R.id.action_makeClubDetailFragment_to_makeClubResultFragment)
