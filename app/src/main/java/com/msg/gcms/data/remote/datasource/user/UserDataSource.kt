e162d90a585d0294ee7435ad0cee0bb8f5aa66f4
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-09 09:21:42 +0900

    ✨ :: profile image 가져오는 로직 datasource 추가

--- app/src/main/java/com/msg/gcms/data/remote/datasource/user/UserDataSource.kt
+++ app/src/main/java/com/msg/gcms/data/remote/datasource/user/UserDataSource.kt
@@ -1,6 +1,7 @@
 package com.msg.gcms.data.remote.datasource.user
 
 import com.msg.gcms.data.remote.dto.user.get_my_profile.GetMyProfileResponse
+import com.msg.gcms.data.remote.dto.user.get_profile_image.GetProfileImageResponse
 import com.msg.gcms.data.remote.dto.user.modify_profile_image.ModifyProfileImageRequest
 import com.msg.gcms.data.remote.dto.user.search_user.GetSearchUserResponse
 
@@ -8,6 +9,8 @@ interface UserDataSource {
     suspend fun getUserInfo(): GetMyProfileResponse
 
     suspend fun putProfile(body: ModifyProfileImageRequest)
+
+    suspend fun getProfileImage(): GetProfileImageResponse
 
     suspend fun getUserSearch(QueryString: Map<String,String>): List<GetSearchUserResponse>
 
