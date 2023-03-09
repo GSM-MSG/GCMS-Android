2ef95ece70bb60e409724d858a521ffd29e3c4c0
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-09 09:29:36 +0900

    ✨ :: add mapper to data for transform data between tiers

--- app/src/main/java/com/msg/gcms/data/mapper/UserMapper.kt
+++ app/src/main/java/com/msg/gcms/data/mapper/UserMapper.kt
@@ -2,9 +2,11 @@ package com.msg.gcms.data.mapper
 
 import com.msg.gcms.data.remote.dto.user.get_my_profile.GetMyProfileResponse
 import com.msg.gcms.data.remote.dto.user.get_my_profile.ProfileClubResponse
+import com.msg.gcms.data.remote.dto.user.get_profile_image.GetProfileImageResponse
 import com.msg.gcms.data.remote.dto.user.search_user.GetSearchUserResponse
 import com.msg.gcms.domain.data.user.get_my_profile.GetMyProfileData
 import com.msg.gcms.domain.data.user.get_my_profile.ProfileClubData
+import com.msg.gcms.domain.data.user.get_profile_image.GetProfileImageData
 import com.msg.gcms.domain.data.user.search_user.GetSearchUserData
 
 object UserMapper {
@@ -40,6 +42,12 @@ object UserMapper {
             number = data.number,
             profileImg = data.profileImg,
             uuid = data.uuid
+        )
+    }
+
+    fun mapperToGetProfileImageData(data: GetProfileImageResponse): GetProfileImageData {
+        return GetProfileImageData(
+            profileImg = data.profileImg
         )
     }
 }
