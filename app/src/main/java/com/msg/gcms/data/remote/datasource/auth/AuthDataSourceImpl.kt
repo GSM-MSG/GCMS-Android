9fdc1035d4c7006539782d861cb9a2b8a1cbcee1
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-03-12 21:51:46 +0900

    :sparkles: refresh 로직 추가

--- app/src/main/java/com/msg/gcms/data/remote/datasource/auth/AuthDataSourceImpl.kt
+++ app/src/main/java/com/msg/gcms/data/remote/datasource/auth/AuthDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package com.msg.gcms.data.remote.datasource.auth
 
+import com.msg.gcms.data.remote.dto.auth.request.RefreshRequest
 import com.msg.gcms.data.remote.dto.auth.request.SignInRequest
 import com.msg.gcms.data.remote.dto.auth.response.SignInResponse
 import com.msg.gcms.data.remote.network.api.AuthAPI
@@ -9,15 +10,18 @@ import javax.inject.Inject
 class AuthDataSourceImpl @Inject constructor(
     private val service: AuthAPI
 ) : AuthDataSource {
-    override suspend fun postRegistration(body: SignInRequest): SignInResponse {
-        return GCMSApiHandler<SignInResponse>()
-            .httpRequest { service.postSignIn(body) }
+    override suspend fun postRegistration(body: SignInRequest): SignInResponse =
+        GCMSApiHandler<SignInResponse>()
+            .httpRequest { service.postSignIn(body = body) }
             .sendRequest()
-    }
 
-    override suspend fun logout() {
-        return GCMSApiHandler<Unit>()
+    override suspend fun logout() =
+        GCMSApiHandler<Unit>()
             .httpRequest { service.logout() }
             .sendRequest()
-    }
+
+    override suspend fun refresh(header: String, body: RefreshRequest): SignInResponse =
+        GCMSApiHandler<SignInResponse>()
+            .httpRequest { service.refresh(header = header, body = body) }
+            .sendRequest()
 }
