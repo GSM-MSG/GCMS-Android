0ae57df5708066fd86d350a0409f0530d6f61f75
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-20 10:51:01 +0900

    ✨ :: connect datasource logic

--- app/src/main/java/com/msg/gcms/data/local/datasource/club/ClubLocalDataSource.kt
+++ app/src/main/java/com/msg/gcms/data/local/datasource/club/ClubLocalDataSource.kt
@@ -1,5 +1,12 @@
 package com.msg.gcms.data.local.datasource.club
 
+import com.msg.gcms.data.local.entity.ClubEntity
+
 interface ClubLocalDataSource {
 
+    fun getClubData(type: String): List<ClubEntity>
+
+    fun insertClubData(clubData: List<ClubEntity>)
+
+    fun deleteClubData(type: String)
 }
--- app/src/main/java/com/msg/gcms/data/local/datasource/club/ClubLocalDataSourceImpl.kt
+++ app/src/main/java/com/msg/gcms/data/local/datasource/club/ClubLocalDataSourceImpl.kt
@@ -1,8 +1,21 @@
 package com.msg.gcms.data.local.datasource.club
 
+import com.msg.gcms.data.local.dao.ClubDao
+import com.msg.gcms.data.local.entity.ClubEntity
 import javax.inject.Inject
 
 class ClubLocalDataSourceImpl @Inject constructor(
+    private val clubDao: ClubDao
 ): ClubLocalDataSource {
+    override fun getClubData(type: String): List<ClubEntity> {
+        return clubDao.getClubList(type = type)
+    }
 
+    override fun insertClubData(clubData: List<ClubEntity>) {
+        return clubDao.insertClubData(clubEntity = clubData)
+    }
+
+    override fun deleteClubData(type: String) {
+        return clubDao.deleteClubData(type = type)
+    }
 }
