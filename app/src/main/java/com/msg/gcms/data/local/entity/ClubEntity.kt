895890dd8e5f94e7dd00044f6c300c8a851513e0
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-20 09:30:30 +0900

    📝 :: create club info entity

---
+++ app/src/main/java/com/msg/gcms/data/local/entity/ClubEntity.kt
@@ -0,0 +1,14 @@
+package com.msg.gcms.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ClubEntity(
+    @PrimaryKey val clubId: Long,
+    @ColumnInfo(name = "type") val type: String,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "bannerImg") val bannerImg: String,
+)
