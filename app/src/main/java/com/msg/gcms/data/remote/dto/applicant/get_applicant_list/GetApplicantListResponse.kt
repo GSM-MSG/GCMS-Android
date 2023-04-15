252e82ca69387833750148fe17b2ebc9b65a48da
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-04-15 16:12:57 +0900

    ✨️ :: create extension of convert applicant list data between data layer and domain layer

--- app/src/main/java/com/msg/gcms/data/remote/dto/applicant/get_applicant_list/GetApplicantListResponse.kt
+++ app/src/main/java/com/msg/gcms/data/remote/dto/applicant/get_applicant_list/GetApplicantListResponse.kt
@@ -1,6 +1,7 @@
 package com.msg.gcms.data.remote.dto.applicant.get_applicant_list
 
 import com.google.gson.annotations.SerializedName
+import com.msg.gcms.domain.data.applicant.get_applicant_list.GetApplicantListData
 
 data class GetApplicantListResponse(
     @SerializedName("scope")
@@ -8,3 +9,10 @@ data class GetApplicantListResponse(
     @SerializedName("applicantList")
     val applicantList: List<ApplicantListResponse>
 )
+
+fun GetApplicantListResponse.toApplicantListData(): GetApplicantListData {
+    return GetApplicantListData(
+        applicantList = applicantList.map { it.toApplicantListData() },
+        userScope = userScope
+    )
+}
