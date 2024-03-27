d2293d81d8a1f1101406b75180510b3e4b6d82bb
이명훈 <s23065@gsm.hs.kr>
2024-03-27 16:55:46 +0900

    :sparkles: :: attend screen publishing

---
+++ app/src/main/java/com/msg/gcms/presentation/view/attend/AttendActivity.kt
@@ -0,0 +1,13 @@
+package com.msg.gcms.presentation.view.attend
+
+import com.msg.gcms.databinding.ActivityAttendBinding
+import com.msg.gcms.R
+import com.msg.gcms.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+/*@AndroidEntryPoint
+class AttendActivity : BaseActivity<ActivityAttendBinding>(R.layout.activity_attend){
+
+
+
+}*/
