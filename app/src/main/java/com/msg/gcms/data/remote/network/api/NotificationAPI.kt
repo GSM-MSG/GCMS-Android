81d01d65834fa40ede5eb395f0c284a03828e29b
명훈 <s23065@gsm.hs.kr>
2024-04-25 11:29:52 +0900

    📝 :: edit mistake

--- app/src/main/java/com/msg/gcms/data/remote/network/api/NotificationAPI.kt
+++ app/src/main/java/com/msg/gcms/data/remote/network/api/NotificationAPI.kt
@@ -9,33 +9,25 @@ import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
-import retrofit2.http.Path
 
 interface NotificationAPI {
 
     @POST("notification/{club_id}")
     fun writeNotification(
-        @Path("club_id") clubId: Long,
         @Body body: PostWriteNotificationRequest
     )
 
     @GET("notification/{club_id}/all")
-    fun getNoticeList(
-        @Path("club_id") clubId: Long
-    ): GetNotificationListResponse
+    fun getNoticeList(): GetNotificationListResponse
 
     @GET("notification/{id}")
-    fun getDetailNotification(
-        @Path("id") id: Long
-    ): GetDetailNotificationResponse
+    fun getDetailNotification(): GetDetailNotificationResponse
 
     @PATCH("notification/{id}")
     fun patchNotification(
-        @Path("id") id: Long
-    ): PatchModifyNotificationRequest
+        @Body body: PatchModifyNotificationRequest
+    )
 
     @DELETE("notification/{id}")
-    fun deleteNotification(
-        @Path("id") id: Long
-    )
+    fun deleteNotification()
 }
